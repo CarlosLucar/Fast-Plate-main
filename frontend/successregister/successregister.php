9ae43a9fa245b1ecf82cf572f936a120de43695d
Miguel Liu Li <liu4008@hotmail.com>
2021-11-17 23:34:05 -0500

    yessir

--- frontend/successregister/successregister.php
+++ frontend/successregister/successregister.php
@@ -10,6 +10,9 @@
 </head>
 
 <body>
+    <img src="welcome.jpg" width="500" height="400">
+    <h1>Now! You are one of our member!</h1>
+    <h5>Click the link below to the main page</h5>
     <div id="click">
         <a href="/index.php">Click here to main page</a>
     </div>
