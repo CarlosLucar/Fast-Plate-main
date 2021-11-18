babf6bede0adb10fe09f9c643e1e2bddf6d8569e
Miguel Liu Li <liu4008@hotmail.com>
2021-11-17 22:15:42 -0500

    f

--- backend/register/register.php
+++ backend/register/register.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($run) {
 
             echo "<p>Still waiting to be proceed......</p>";
-            header("Refresh:3, URL=/index.php");
+            header("Refresh:3, URL=/frontend/successregister/successregister.php");
         } else {
             echo "<h1>Error!</h1><p>You could not be regitered, please try again</p>";
             echo "<p>" . mysqli_error($dbc) . "</p>";
