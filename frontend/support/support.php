2a924e907defa8d13fbb9787d16a13ed554b95a5
Miguel Liu Li <liu4008@hotmail.com>
2021-12-01 14:26:12 -0500

    support

---
+++ frontend/support/support.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+</head>
+
+<body>
+    <section class="contact">
+        <div class="content">
+            <h1>FastPlate</h1>
+            <h2>Contact us</h2>
+            <p>
+                Please Type anything that you concerning on our service. we will be so Happy to response back to you!
+            </p>
+        </div>
+        <div class="container">
+            <div class="contactinfo">
+                <div class="box">
+                    <div class="icon"><i class="fa fa-map-marker fa-3x" aria-hidden="true"></i></div>
+                    <div class="text">
+                        <h3>Address</h3>
+                        <p>3838 boot Camp Road, <br>Brooklyn, New York,<br>11314</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon"><i style="font-size:36px" class="fa">&#xf098;</i></div>
+                    <div class="text">
+                        <h3>Phone</h3>
+                        <p>718-968-5618</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></div>
+                    <div class="text">
+                        <h3>Email</h3>
+                        <p>fastplate@deliver.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="contactform">
+                <form action="/frontend/support/ty.php">
+
+                    <h2>Contact Support</h2>
+                    <div class="inputbox">
+                        <!-- <span>Full Name</span> -->
+                        <input type="text" name="" required="required" placeholder="Full Name">
+
+                    </div>
+                    <div class="inputbox">
+                        <input type="text" name="" required="required" placeholder="Email">
+                        <!-- <span>Email</span> -->
+                    </div>
+                    <div class="inputbox">
+                        <textarea required="required" placeholder="Type you question or comment"></textarea>
+                        <!-- <span>Type Your comment or question......</span> -->
+                    </div>
+                    <div class="inputbox">
+                        <input type="submit" name="" value="send">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ frontend/support/ty.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <img src="image/welcome.jpg" width="500" height="400">
+    <h1>Thank you for sending us your concern.</h1>
+    <h2>We will review your concerning and get back to you soon!</h2>
+    <div id="click">
+        <a href="/index.php">Click here to main page</a>
+    </div>
+</body>
+
+</html>
