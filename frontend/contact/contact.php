976fb6fea5174d30734c6dcd3b66450beafc3fea
KatieDev <kitkatie117@gmail.com>
2021-12-09 18:15:26 -0500

    partner

--- frontend/contact/contact.php
+++ frontend/contact/contact.php
@@ -10,7 +10,9 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;600;700&family=Nunito:wght@400;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+
     <title>Become a Partner</title>
+    <link rel="shortcut icon" type="image/png" href="img/logo.png">
 </head>
 
 <body>
@@ -38,44 +40,84 @@
         </div>
     </nav>
 
-
-
-
-
+<div class="partner-form">
     <form action="/frontend/contact/ty.php" method="post">
-        <h2>Became Partner</h2>
+        <h2>Become a Partner</h2>
         <label for="">First Name</label>
-        <input type="text" name="fname" placeholder="User Name"><br>
+        <input type="text" name="fname" placeholder="First Name"><br>
 
         <label for="lname">Last Name</label>
-        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+        <input type="text" id="lname" name="lastname" placeholder="Last Name"><br>
 
         <label for="email">Your Email</label>
-        <input type="text" id="email" name="emal" placeholder="Your last name..">
+        <input type="text" id="email" name="emal" placeholder="Email"><br>
 
+        <label for="state">State</label>
         <br>
-        <label for="country">Country</label>
-        <br>
-        <br>
-        <select id="country" name="country">
+
+        <select id="state" name="state">
             <option value="australia">New York</option>
             <option value="canada">Florida</option>
             <option value="usa">California</option>
-            <option value="australia">Gerogia</option>
-            <option value="canada">Chicago</option>
+            <option value="australia">Georgia</option>
+            <option value="canada">Arizona</option>
             <option value="usa">Washington</option>
             <option value="australia">Oregon</option>
             <option value="canada">Texas</option>
-            <option value="usa">Tenesse</option>
+            <option value="usa">Tenessee</option>
         </select>
         <br>
         <br>
-        <label for="subject">Explain why you want to partner with us:</label>
+        <label for="subject">Explain why you would like to partner with us:</label>
         <br>
-        <br>
-        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-        <button type="submit">Submit</button>
+        <textarea id="subject" name="subject" placeholder="Enter comment here" style="height:200px"></textarea>
+        <button type="submit" class="btn btn-primary btn-lg">Submit</button>
     </form>
+</div>
+
+<div class="footer-container">
+        <div class="footer">
+            <div class="footer-heading footer-1">
+                <h2>About Us</h2>
+                <a href="#">About us</a>
+                <a href="#">Add your restaurant</a>
+                <a href="#">Apply to be our deliver</a>
+                <a href="#">Customer Service</a>
+                <a href="#">Terms of services</a>
+            </div>
+            <div class="footer-heading footer-2">
+                <h2>Contact Us</h2>
+                <a href="#">Contact us</a>
+                <a href="#">Support</a>
+                <a href="#">Sponsorship</a>
+                <a href="#">Investors</a>
+                <a href="#">Terms of services</a>
+            </div>
+            <div class="footer-heading footer-3">
+                <h2>Social Media</h2>
+                <a href="#">Instagram</a>
+                <a href="#">Facebook</a>
+                <a href="#">Twitter</a>
+                <a href="#">Youtube</a>
+            </div>
+            <div class="footer-email-form">
+                <h2>Join our newsletter</h2>
+                <input type="email" plcaeholder="Enter your email address" id="footer-email">
+                <input type="submit" value="Sign up" class="btn btn-primary btn-lg">
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="menu.js"></script>
+
+    <script src="cart.js"></script>
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </html>
