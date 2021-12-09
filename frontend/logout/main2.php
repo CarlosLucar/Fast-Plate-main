c915d3ed4647fee61452ee8cbf5646e8cf9e1f52
Carlos Lucar <lucar.carlos1@gmail.com>
2021-12-09 15:03:54 -0500

    Logged in Nav bar Update

--- frontend/logout/main2.php
+++ frontend/logout/main2.php
@@ -5,37 +5,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;600;700&family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <title>Fastplate:Home</title>
-    <link rel="shortcut icon" type="image/png" href="/img/fastplate.png">
+    
 </head>
 
 <body>
     <!-- <a href="/fastplate/frontend/login/index.php">Login</a><br>
     <a href="/fastplate/frontend/register/registerform.php">Register</a>
     <h1>Welcome To The Main Page</h1> -->
-    <div class="menu-bar">
-        <ul>
-            <li class="logo"><i class="w3-margin-left fa fa-home"></i><a href="#"> fastplate</a></li>
+    <nav class="navbar navbar-expand-md navbar-light">
+        <a class="navbar-brand" href="/index.php"><img src="img/logo.png" alt="logo" width="60"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <li class="menu"><a href="#">Food Menu</a>
-                <div class="submenu">
-                    <ul>
-                        <li><a href="#">Sushi</a></li>
-                        <li><a href="#">Drink</a></li>
-                        <li><a href="#">Vegetable</a></li>
-                    </ul>
-                </div>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item brand">
+                <a class="nav-link brand" href="/index.php">FastPlate</a>
             </li>
-            <li class="menu"><a href="#">About Us</a></li>
-            <li class="menu"><a href="#">Become Partner</a></li>
-            <li class="menu"><a href="#">Contact support</a></li>
-            <li class="menu"><a href="#">Register</a></li>
-            <li class="menu"><a href="/backend/logout/logout.php">Log out</a></li>
         </ul>
-    </div>
+        <ul class="navbar-nav">
+            <li class="nav-item menu"><a class="nav-link" href="/frontend/aboutus/aboutus.php">About Us</a></li>
+            <li class="nav-item menu"><a class="nav-link" href="/frontend/support/support.php">Contact</a></li>
+            <li class="nav-item menu"><a class="nav-link" href="/frontend/contact/contact.php">Become a Partner</a></li>
+            <li class="nav-item menu"><a class="btn btn-primary btn-lg" href="/backend/logout/logout.php">Log out</a></li>
+        </ul>
+        </div>
+    </nav>
 </body>
 
 </html>
+
