a5a6496cab7ceaf0691f32b5f66676de68f4b0c8
KatieDev <kitkatie117@gmail.com>
2021-12-09 18:47:26 -0500

    ty

--- frontend/successregister/successregister.php
+++ frontend/successregister/successregister.php
@@ -2,20 +2,90 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;600;700&family=Nunito:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+
+    <title>Thank You!</title>
+    <link rel="shortcut icon" type="image/png" href="logo.png">
+
 </head>
 
 <body>
+<nav class="navbar navbar-expand-md navbar-light">
+        <a class="navbar-brand" href="/index.php"><img src="logo.png" alt="logo" width="60"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item brand">
+                <a class="nav-link brand" href="/index.php">FastPlate</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item menu"><a class="nav-link" href="/frontend/aboutus/aboutus.php">About Us</a></li>
+            <li class="nav-item menu"><a class="nav-link" href="/frontend/support/support.php">Contact</a></li>
+            <li class="nav-item menu"><a class="nav-link" href="/frontend/contact/contact.php">Become a Partner</a></li>
+            <li class="nav-item menu"><a class="nav-link" href="/frontend/login/fLogin.php">Login</a></li>
+            <li class="nav-item menu"> <a class="btn btn-primary btn-lg" href="/frontend/register/registerform.php" role="button">Sign Up</a></li>
+        </ul>
+        </div>
+    </nav>
+
+    <div class='content'>
     <img src="welcome.jpg" width="500" height="400">
-    <h1>Now! You are one of our member!</h1>
-    <h5>Click the link below to the main page</h5>
-    <div id="click">
-        <a href="/index.php">Click here to main page</a>
     </div>
+    <h1>Thank you for joining us!</h1>
+
+    <div class="footer-container">
+        <div class="footer">
+            <div class="footer-heading footer-1">
+                <h2>About Us</h2>
+                <a href="#">About us</a>
+                <a href="#">Add your restaurant</a>
+                <a href="#">Apply to be our deliver</a>
+                <a href="#">Customer Service</a>
+                <a href="#">Terms of services</a>
+            </div>
+            <div class="footer-heading footer-2">
+                <h2>Contact Us</h2>
+                <a href="#">Contact us</a>
+                <a href="#">Support</a>
+                <a href="#">Sponsorship</a>
+                <a href="#">Investors</a>
+                <a href="#">Terms of services</a>
+            </div>
+            <div class="footer-heading footer-3">
+                <h2>Social Media</h2>
+                <a href="#">Instagram</a>
+                <a href="#">Facebook</a>
+                <a href="#">Twitter</a>
+                <a href="#">Youtube</a>
+            </div>
+            <div class="footer-email-form">
+                <h2>Join our newsletter</h2>
+                <input type="email" plcaeholder="Enter your email address" id="footer-email">
+                <input type="submit" value="Sign up" class="btn btn-primary btn-lg">
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="menu.js"></script>
+
+    <script src="cart.js"></script>
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </html>
